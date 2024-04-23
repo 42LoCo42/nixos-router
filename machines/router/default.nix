d43152e7481783a7e8de35edc7542d6308bd2073
Leon Schumacher <leonsch@protonmail.com>
2024-04-23 13:41:44 +0200

    Switch to dnsmasq+stubby

--- machines/router/default.nix
+++ machines/router/default.nix
@@ -3,6 +3,17 @@ let
   wanIF = "enp1s0";
   lanIF = "wlp2s0u1";
   lanIP = "10.0.0.1";
+
+  netbootxyz-kpxe =
+    let
+      src = pkgs.fetchurl {
+        url = "https://github.com/netbootxyz/netboot.xyz/releases/download/2.0.78/netboot.xyz.kpxe";
+        hash = "sha256-pWbhaS3m4kMOlCH8MyDlX7mM6y0udCxYQAVw9al6Jxs=";
+      };
+    in
+    pkgs.runCommand "netbootxyz-kpxe" { } ''
+      install -Dm444 "${src}" "$out/netboot.xyz.kpxe"
+    '';
 in
 {
   aquaris = {
@@ -24,30 +35,24 @@ in
         "nixos/persist" = { };
       };
     };
+
+    persist.system = [
+      "/var/lib/dnsmasq"
+      "/var/lib/systemd"
+    ];
   };
 
   users.users.admin.openssh.authorizedKeys.keys =
     [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVieLCkWGImVI9c7D0Z0qRxBAKf0eaQWUfMn0uyM/Ql" ];
 
-  boot.kernel.sysctl = {
-    "net.ipv4.conf.all.forwarding" = true;
-    "net.ipv4.ip_unprivileged_port_start" = 0;
-    "net.ipv6.conf.all.disable_ipv6" = 1;
-  };
+  boot.kernel.sysctl."net.ipv4.conf.all.forwarding" = true;
 
   hardware.firmware = with pkgs; [ ath9k-htc-blobless-firmware ];
 
   networking = {
+    useDHCP = false;
     firewall.enable = false;
     networkmanager.enable = lib.mkForce false;
-
-    nameservers = [
-      "1.1.1.1#one.one.one.one"
-      "1.0.0.1#one.one.one.one"
-
-      "9.9.9.9#dns.quad9.net"
-      "149.112.112.112#dns.quad9.net"
-    ];
 
     interfaces = {
       ${wanIF}.useDHCP = true;
@@ -70,32 +75,10 @@ in
   };
 
   systemd.network.wait-online.enable = lib.mkForce true;
+  systemd.services.dnsmasq.after = [ "network-online.target" ];
 
   services = {
-    kea.dhcp4 = {
-      enable = true;
-      settings = {
-        interfaces-config.interfaces = [ lanIF ];
-        subnet4 = [{
-          subnet = "10.0.0.0/24";
-          pools = [{ pool = "10.0.0.2 - 10.0.0.254"; }];
-          option-data = [
-            { name = "routers"; data = lanIP; }
-            { name = "domain-name-servers"; data = lanIP; }
-          ];
-        }];
-      };
-    };
-
-    resolved = {
-      llmnr = "false";
-      dnssec = "true";
-      dnsovertls = "true";
-      fallbackDns = [ ];
-      extraConfig = ''
-        DNSStubListenerExtra=${lanIP}
-      '';
-    };
+    resolved.enable = false;
 
     hostapd = {
       enable = true;
@@ -105,6 +88,56 @@ in
           authentication.saePasswordsFile =
             config.aquaris.secrets."machine/sae-password".outPath;
         };
+      };
+    };
+
+    stubby = {
+      enable = true;
+      settings = pkgs.stubby.settingsExample // {
+        listen_addresses = [ "127.0.0.1@53000" ];
+        dnssec_return_status = "GETDNS_EXTENSION_TRUE";
+        upstream_recursive_servers = [
+          { address_data = "1.1.1.1"; tls_port = 853; tls_auth_name = "cloudflare-dns.com"; }
+          { address_data = "1.0.0.1"; tls_port = 853; tls_auth_name = "cloudflare-dns.com"; }
+          { address_data = "9.9.9.9"; tls_port = 853; tls_auth_name = "dns.quad9.net"; }
+          { address_data = "149.112.112.112"; tls_port = 853; tls_auth_name = "dns.quad9.net"; }
+        ];
+      };
+    };
+
+    dnsmasq = {
+      enable = true;
+      settings = {
+        # listen on localhost and LAN
+        interface = lanIF;
+        bind-interfaces = true;
+        listen-address = [ "127.0.0.1" lanIP ];
+
+        # forward to stubby
+        no-resolv = true;
+        server = [ "127.0.0.1#53000" ];
+
+        # misc
+        cache-size = 10000;
+        proxy-dnssec = true;
+        log-queries = true;
+
+        # local domain
+        local = "/lan/";
+        domain = "lan";
+
+        # DHCP
+        dhcp-range = "10.0.0.2,10.0.0.254,12h";
+        dhcp-option = [
+          "option:router,${lanIP}"
+          "option:dns-server,${lanIP}"
+          "option:tftp-server,${lanIP}"
+        ];
+
+        # netboot.xyz
+        enable-tftp = true;
+        tftp-root = netbootxyz-kpxe.outPath;
+        dhcp-boot = "netboot.xyz.kpxe";
       };
     };
   };
